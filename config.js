cb391367ceac2310d830f5cb641d3fa928460998
Dpk417182 <derrickkeough@yahoo.com>
2022-04-03 12:09:05 -0400

    firstButtonsContainer

--- config.js
+++ config.js
@@ -50,42 +50,81 @@ const CONFIG = {
 	// ├┴┐│ │ │  │ │ ││││└─┐
 	// └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
 
+	// firstButtonsContainer: [
+	// 	{
+	// 		id: '1',
+	// 		name: 'Github',
+	// 		icon: 'github',
+	// 		link: 'https://github.com/',
+	// 	},
+	// 	{
+	// 		id: '2',
+	// 		name: 'Mail',
+	// 		icon: 'mail',
+	// 		link: 'https://mail.protonmail.com/',
+	// 	},
+	// 	{
+	// 		id: '3',
+	// 		name: 'Todoist',
+	// 		icon: 'trello',
+	// 		link: 'https://todoist.com',
+	// 	},
+	// 	{
+	// 		id: '4',
+	// 		name: 'Calendar',
+	// 		icon: 'calendar',
+	// 		link: 'https://calendar.google.com/calendar/r',
+	// 	},
+	// 	{
+	// 		id: '5',
+	// 		name: 'Reddit',
+	// 		icon: 'glasses',
+	// 		link: 'https://reddit.com',
+	// 	},
+	// 	{
+	// 		id: '6',
+	// 		name: 'Odysee',
+	// 		icon: 'youtube',
+	// 		link: 'https://odysee.com/',
+	// 	},
+	// ],
+
 	firstButtonsContainer: [
 		{
 			id: '1',
-			name: 'Github',
-			icon: 'github',
-			link: 'https://github.com/',
+			name: 'YouTube',
+			icon: 'youtube',
+			link: 'https://youtube.com/',
 		},
 		{
 			id: '2',
-			name: 'Mail',
-			icon: 'mail',
-			link: 'https://mail.protonmail.com/',
+			name: 'Reddit',
+			icon: 'glasses',
+			link: 'https://reddit.com/',
 		},
 		{
 			id: '3',
-			name: 'Todoist',
-			icon: 'trello',
-			link: 'https://todoist.com',
+			name: 'Whatsapp',
+			icon: 'message-circle',
+			link: 'https://web.whatsapp.com',
 		},
 		{
 			id: '4',
-			name: 'Calendar',
-			icon: 'calendar',
-			link: 'https://calendar.google.com/calendar/r',
+			name: 'Yahoo',
+			icon: 'mail',
+			link: 'https://yahoo.com',
 		},
 		{
 			id: '5',
-			name: 'Reddit',
-			icon: 'glasses',
-			link: 'https://reddit.com',
+			name: 'Netflix',
+			icon: 'play',
+			link: 'https://netflix.com',
 		},
 		{
 			id: '6',
-			name: 'Odysee',
-			icon: 'youtube',
-			link: 'https://odysee.com/',
+			name: 'Monkeytype',
+			icon: 'keyboard',
+			link: 'https://monkeytype.com/',
 		},
 	],
 
